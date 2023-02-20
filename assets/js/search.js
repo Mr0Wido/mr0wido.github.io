e69b40c738a463052184fc295c1dbb2c865161a9
Furkan Deniz <mr0wido@protonmail.com>
2023-02-21 02:20:34 +0300

    New

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,22 +1,10 @@
-function search(event) {
-  event.preventDefault(); // form submissionunu önler
-  var searchTerm = $("#search-input").val(); // kullanıcının girdiği kelimeyi alır
-  $.ajax({
-    url: "/search.json", // veri kaynağı dosyasının yolu
-    dataType: "json",
-    success: function(data) {
-      var filteredData = data.filter(function(item) {
-        // burada filtreleme işlemi gerçekleştirilir
-        return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 || item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
-      });
-      var topResults = filteredData.slice(0, 3); // en yakın 3 sonucu alır
-      var resultList = $("<ul>"); // sonuçları liste olarak görüntüler
-      topResults.forEach(function(result) {
-        resultList.append($("<li>").text(result.title)); // örneğin sadece başlıkları listeleriz
-      });
-      $(".search-results").html(resultList); // sonuçları navbar'ın altında görüntüler
-    }
-  });
-}
-
-$("#search-form").on("submit", search); // form submit edildiğinde search fonksiyonunu çağırır
+window.simpleJekyllSearch = new SimpleJekyllSearch({
+        searchInput: document.getElementById('search-input'),
+        resultsContainer: document.getElementById('results-container'),
+        json: '/search.json',
+        searchResultTemplate: '<li><a href="{url}?query={query}" title="{desc}">{title}</a></li>',
+        noResultsText: 'No results found',
+        limit: 10,
+        fuzzy: false,
+        exclude: ['Welcome']
+    })
